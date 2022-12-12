04b4ddf19ca7c351e5ffba677633aff3cb460538
bl4ck-orig <seho1996@yahoo.de>
2022-12-12 01:46:03 +0100

    Push scripts

---
+++ AStar.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class AStar
+{
+    static int xLength = 20;
+    static int yLength = 10;
+    static int obstacles = 10;
+    static int seed = 0;
+
+    static Node startNode, endNode;
+    static Node[,] nodes;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("X start:");
+        int xStart = int.Parse(Console.ReadLine());
+        Console.WriteLine("Y start:");
+        int yStart = int.Parse(Console.ReadLine());
+        Console.WriteLine("X end:");
+        int xEnd = int.Parse(Console.ReadLine()); 
+        Console.WriteLine("Y end:");
+        int yEnd = int.Parse(Console.ReadLine());
+
+        endNode = new Node(xEnd, yEnd);
+        startNode = new Node(xStart, yStart);
+
+        CreateMatrix();
+
+        try
+        {
+            List<Node> path = AStart();
+            PrintMatrix(path);
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Path not possible!");
+            PrintMatrix(new List<Node>());
+        }
+
+        Console.ReadLine();
+    }
+
+    static void CreateMatrix()
+    {
+        nodes = new Node[xLength, yLength];
+
+        for (int x = 0; x < xLength; x++)
+            for (int y = 0; y < yLength; y++)
+                nodes[x, y] = new Node(x, y);
+
+        Random rnd = new Random(seed);
+        for (int i = 0; i < obstacles; i++)
+        {
+            int nX = rnd.Next(xLength);
+            int nY = rnd.Next(yLength);
+
+            Node n = nodes[nX, nY];
+            if (!n.Obs && n != startNode && n != endNode)
+                nodes[nX, nY].Obs = true;
+        }
+    }
+
+    static void PrintMatrix(List<Node> _p)
+    {
+        for (int y = 0; y < yLength; y++)
+        {
+            for (int x = 0; x < xLength; x++)
+                Console.Write(((nodes[x, y].Obs) ? 'O' : ((_p.Contains(nodes[x, y])) ? 'X' : '*')));
+
+            Console.WriteLine();
+        }
+    }
+
+    static List<Node> AStart()
+    {
+        List<Node> openNodes = new List<Node>();
+        openNodes.Add(startNode);
+
+        startNode.G = 0f;
+        startNode.SetT();
+
+        nodes[startNode.X, startNode.Y] = startNode;
+        nodes[endNode.X, endNode.Y] = endNode;
+
+        for(Node currentNode = null; openNodes.Count != 0; endNode.CameFrom = currentNode)
+        {
+            currentNode = openNodes.OrderBy(x => x.T).First();
+
+            if (currentNode == endNode)
+                return SetPath(endNode);
+
+            openNodes.Remove(currentNode);
+
+            float nextT = 0f;
+            foreach (var neighbour in GetNeighbours(currentNode).Where(x => currentNode.GetStepCost(x, out float stepCost) && (nextT = currentNode.G + stepCost) < x.G))
+            {
+                neighbour.CameFrom = currentNode;
+                neighbour.G = nextT;
+                neighbour.SetT();
+            
+                if (!openNodes.Contains(neighbour))
+                    openNodes.Add(neighbour);
+            }
+        }
+
+        throw new InvalidOperationException();
+    }
+
+    static List<Node> SetPath(Node _eN)
+    {
+        List<Node> path = new List<Node>();
+
+        for (; _eN != null; _eN = _eN.CameFrom)
+            path.Add(_eN);
+
+        return path;
+    }
+
+    private static List<Node> GetNeighbours(Node _n)
+    {
+        List<Node> neighbours = new List<Node>();
+
+        for (int x = Math.Max(_n.X - 1, 0); x <= Math.Min(_n.X + 1, xLength - 1); x++)
+            for (int y = Math.Max(_n.Y - 1, 0); y <= Math.Min(_n.Y + 1, yLength - 1); y++)
+                if (x != _n.X || y != _n.Y)
+                    neighbours.Add(nodes[x, y]);
+
+        return neighbours;
+    }
+
+    private class Node
+    {
+        float vertCost = 1f;
+        float diagCost = 1.4f;
+
+        public int X, Y;
+        public float T, G = float.MaxValue;
+        public float H;
+        public bool Obs;
+        public Node CameFrom;
+
+        public Node(int _x, int _y)
+        {
+            X = _x;
+            Y = _y;
+            Obs = false;
+            SetH();
+        }
+
+        public void SetH()
+        {
+            if (endNode == null)
+                return;
+
+            int xDist = Math.Abs(X - endNode.X);
+            int yDist = Math.Abs(Y - endNode.Y);
+
+            int verts = Math.Abs(xDist - yDist);
+
+            H = verts * vertCost + (Math.Max(xDist, yDist) - verts) * diagCost;
+        }
+
+        public void SetT() => T = G + H;
+
+        public bool GetStepCost(Node _nN, out float _c)
+        {
+            _c = (X == _nN.X || Y == _nN.Y) ? vertCost : diagCost;
+            return !_nN.Obs;
+        }
+    }
+}
---
+++ AStarClear.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class AStarClear
+{
+    static int xSize = 20;
+    static int ySize = 10;
+    static int obstacles = 100;
+    static int seed = 0;
+    static List<Node> openNodes = new List<Node>();
+    static Node startNode;
+    static Node endNode;
+    static Node[,] nodeMatrix;
+
+    static char pathChar = 'X';
+    static char obstacleChar = 'O';
+    static char emptyChar = '*';
+
+    static void Main(string[] args)
+    {
+        int xStart, yStart, xEnd, yEnd;
+
+        do
+            Console.WriteLine("Enter x start:");
+        while (!int.TryParse(Console.ReadLine(), out xStart));
+            
+        do
+            Console.WriteLine("Enter y start:");
+        while (!int.TryParse(Console.ReadLine(), out yStart));
+            
+        do
+            Console.WriteLine("Enter x end:");
+        while (!int.TryParse(Console.ReadLine(), out xEnd));
+            
+        do
+            Console.WriteLine("Enter y end:");
+        while (!int.TryParse(Console.ReadLine(), out yEnd));
+
+        endNode = new Node(xEnd, yEnd, null);
+        startNode = new Node(xStart, yStart, endNode);
+
+        CreateMatrix();
+
+        try
+        {
+            List<Node> path = AStart();
+            PrintMap(path);
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Path not possible!");
+            PrintMap(new List<Node>());
+        }
+
+        Console.ReadLine();
+    }
+
+    static void CreateMatrix()
+    {
+        nodeMatrix = new Node[xSize, ySize];
+
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                nodeMatrix[x, y] = new Node(x, y, endNode);
+            }
+        }
+
+        Random random = new Random(seed);
+        for (int i = 0; i < obstacles; i++)
+        {
+            int nextObstacleX = random.Next(xSize);
+            int nextObstacleY = random.Next(ySize);
+            if (nodeMatrix[nextObstacleX, nextObstacleY].IsObstacle)
+            {
+                i--;
+                continue;
+            }
+
+            if(nodeMatrix[nextObstacleX, nextObstacleY] == startNode)
+            {
+                i--;
+                continue;
+            }
+
+            if (nodeMatrix[nextObstacleX, nextObstacleY] == endNode)
+            {
+                i--;
+                continue;
+            }
+
+            nodeMatrix[nextObstacleX, nextObstacleY].SetObstacle();
+        }
+    }
+
+    static void PrintMap(List<Node> _path)
+    {
+        for (int y = 0; y < ySize; y++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                if (nodeMatrix[x, y].IsObstacle)
+                    Console.Write(obstacleChar);
+                else if (_path.Contains(nodeMatrix[x,y]))
+                    Console.Write(pathChar);
+                else
+                    Console.Write(emptyChar);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static List<Node> AStart()
+    {
+        startNode.SetCurrentCost(0f);
+        startNode.RecalcTotalCost();
+        nodeMatrix[startNode.X, startNode.Y] = startNode;
+        nodeMatrix[endNode.X, endNode.Y] = endNode;
+        openNodes.Add(startNode);
+
+        Node currentNode = null;
+
+        while(openNodes.Count != 0)
+        {
+            Node nextNode = openNodes.OrderBy(x => x.TotalCost).First();
+
+            if (nextNode == endNode)
+            {
+                startNode.SetCameFrom(null);
+                endNode.SetCameFrom(currentNode);
+                return CreatePath(endNode);
+            }
+            currentNode = nextNode;
+
+            openNodes.Remove(currentNode);
+
+            List<Node> neighbourNodes = GetNeighbourNodes(currentNode);
+
+            for (int i = 0; i < neighbourNodes.Count; i++)
+            {
+                if (!currentNode.TryGetCost(neighbourNodes[i], out float neighBourCost))
+                    continue;
+
+                float nextTotalCost = currentNode.CurrentCost + neighBourCost;
+
+                if (nextTotalCost >= neighbourNodes[i].CurrentCost)
+                    continue;
+
+                neighbourNodes[i].SetCameFrom(currentNode);
+                neighbourNodes[i].SetCurrentCost(nextTotalCost);
+                neighbourNodes[i].RecalcTotalCost();
+
+                if (!openNodes.Contains(neighbourNodes[i]))
+                    openNodes.Add(neighbourNodes[i]);
+            }
+        }
+
+        throw new InvalidOperationException();
+    }
+
+    private static List<Node> CreatePath(Node _endNode)
+    {
+        List<Node> path = new List<Node>();
+
+        Node currentNode = _endNode;
+        while (currentNode.CameFrom != null)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.CameFrom;
+        }
+        path.Add(currentNode);
+
+        return path;
+    }
+
+    private static List<Node> GetNeighbourNodes(Node _currentNode)
+    {
+        List<Node> nodes = new List<Node>();
+
+        int xMin = Math.Max(_currentNode.X - 1, 0);
+        int xMax = Math.Min(_currentNode.X + 1, xSize - 1);
+        int yMin = Math.Max(_currentNode.Y - 1, 0);
+        int yMax = Math.Min(_currentNode.Y + 1, ySize - 1);
+
+        for (int x = xMin; x <= xMax; x++)
+        {
+            for (int y = yMin; y <= yMax; y++)
+            {
+                if (x != _currentNode.X || y != _currentNode.Y)
+                    nodes.Add(nodeMatrix[x, y]);
+            }
+        }
+
+        return nodes;
+    }
+
+    private class Node
+    {
+        public const float vertCost = 1f;
+        public const float diagCost = 1.4f;
+
+        public int X { get; }
+        public int Y { get; }
+        public bool IsObstacle { get; private set; }
+        public float TotalCost { get; private set; } = float.MaxValue;
+        public float CurrentCost { get; private set; } = float.MaxValue;
+        public float HeuristicCost { get; private set; }
+        public Node CameFrom { get; private set; }
+
+
+        public Node(int _x, int _y, Node _endNode)
+        {
+            X = _x;
+            Y = _y;
+            IsObstacle = false;
+            SetHeuristicCost(_endNode);
+        }
+
+        public void SetHeuristicCost(Node _endNode)
+        {
+            if (_endNode == null)
+            {
+                HeuristicCost = 0;
+                return;
+            }
+
+            int xDistance = Math.Abs(X - _endNode.X);
+            int yDistance = Math.Abs(Y - _endNode.Y);
+
+            int verts = Math.Abs(xDistance - yDistance);
+            int diags = Math.Max(xDistance, yDistance) - verts;
+            HeuristicCost = verts * vertCost + diags * diagCost;
+        }
+
+        public void RecalcTotalCost()
+        {
+            TotalCost = CurrentCost + HeuristicCost;
+        }
+
+        public void SetCurrentCost(float _cost)
+        {
+            CurrentCost = _cost;
+        }
+
+        public void SetCameFrom(Node _cameFrom) => CameFrom = _cameFrom;
+
+        public bool TryGetCost(Node _neighBourNode, out float _cost)
+        {
+            _cost = 0;
+            if (_neighBourNode.IsObstacle)
+                return false;
+
+            if (X == _neighBourNode.X || Y == _neighBourNode.Y)
+            {
+                _cost = vertCost;
+                return true;
+            }
+
+            _cost = diagCost;
+            return true;
+        }
+
+        public void SetObstacle() => IsObstacle = true;
+    }
+}
